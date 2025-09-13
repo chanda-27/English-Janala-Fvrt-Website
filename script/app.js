716831a75534e9f45c99b82cf946e0223ac0b87a
chanda-27 <chandabiswas27@gmail.com>
2025-09-13 15:29:00 +0600

    added some card design change

--- script/app.js
+++ script/app.js
@@ -15,15 +15,35 @@ const loadLevelWord = (id) => {
 const displayLearn = (words) => {
   const wordGetContainer = document.getElementById("word-container");
   wordGetContainer.innerHTML = "";
+  if (words.length === 0) {
+    wordGetContainer.innerHTML = `
+     <div class="col-span-full text-center space-y-4">
+          <img class="mx-auto" src="./assets/alert-error.png">
+          <p class="text-gray-600 font-bangla">
+            এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+          </p>
+          <h1 class="text-4xl font-semibold font-bangla">
+            নেক্সট Lesson এ যান
+          </h1>
+      </div>
+
+     `;
+  }
   for (let word of words) {
     const wordCard = document.createElement("div");
     wordCard.innerHTML = `
       <div
           class="card bg-[#ffffff] shadow-sm rounded-lg py-14 px-6 text-center space-y-4"
         >
-          <h1 class="text-2xl font-bold">${word.word}</h1>
-          <p class="font-semibold">This is too much and proggraming</p>
-          <h2 class="text-2xl font-medium font-bangla">${word.meaning}/${word.pronunciation}</h2>
+          <h1 class="text-2xl font-bold">${
+            word.word ? word.word : "শব্দ পাওয়া যাইনি"
+          }</h1>
+          <p class="font-semibold">Meaning / Pronunciation</p>
+          <h2 class="text-2xl font-medium font-bangla">${
+            word.meaning ? word.meaning : "অর্থ পাওয়া যাইনি "
+          } / ${
+      word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যাইনি "
+    }</h2>
           <div class="flex justify-between items-center mt-10">
             <button class="btn bg-purple-300 hover:bg-blue-600">
               <i class="fa-solid fa-circle-info"></i>
